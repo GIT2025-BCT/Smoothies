05bc50987c3c95ea0285db542f313475de38db6b
GIT2025-BCT <premakumari.k@bahwancybertek.com>
2025-05-15 18:20:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,9 @@ import streamlit as st
 
 name_on_order = st.text_input("Name on the Smoothie")
 st.write("The name on the Smoothie is ", name_on_order)
-
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
