92a1abc41dcb7dfe83c96929e333e09f7c1b0667
GIT2025-BCT <premakumari.k@bahwancybertek.com>
2025-05-15 19:07:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,14 +21,14 @@ ingredients_list = st.multiselect(
 	)
 if ingredients_list:  
 	ingredients_string= ''
-for fruit_choosen in ingredients_list:
-	ingredients_string += fruit_choosen + ' '
-	smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-	sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+	for fruit_choosen in ingredients_list:
+		ingredients_string += fruit_choosen + ' '
+		smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+		sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-	my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
-            values ('""" + ingredients_string + """','""" + name_on_order +"""')"""
-	time_to_insert = st.button('Submit Order')
+		my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
+            	values ('""" + ingredients_string + """','""" + name_on_order +"""')"""
+		time_to_insert = st.button('Submit Order')
 if time_to_insert:
     	session.sql(my_insert_stmt).collect()
     	st.success('Your Smoothie is ordered!', icon="✅")
