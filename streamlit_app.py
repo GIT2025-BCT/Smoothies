2e659d4e7bcc5025692772c575e087731cd9b833
GIT2025-BCT <premakumari.k@bahwancybertek.com>
2025-05-15 18:57:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,9 @@ ingredients_list = st.multiselect(
 if ingredients_list:  
 	ingredients_string= ''
 	for fruit_choosen in ingredients_list:
-  	ingredients_string += fruit_choosen + ' '
-	smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-	sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+  		ingredients_string += fruit_choosen + ' '
+		smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+		sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 	my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
             values ('""" + ingredients_string + """','""" + name_on_order +"""')"""
