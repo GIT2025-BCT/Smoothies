fb97600ba0232aad21084a040a9b1d9cdee98edb
GIT2025-BCT <premakumari.k@bahwancybertek.com>
2025-05-15 19:32:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUI
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
             	values ('""" + ingredients_string + """','""" + name_on_order +"""')"""
 		time_to_insert = st.button('Submit Order')
-	if time_to_insert:
-    		session.sql(my_insert_stmt).collect()
-    		st.success('Your Smoothie is ordered!', icon="✅")
 import requests
 if ingredients_list:  
 	ingredients_string= ''
