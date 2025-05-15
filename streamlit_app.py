ae2486a2666ff4aa64aec8f0680c6e14e2ef5f6c
GIT2025-BCT <premakumari.k@bahwancybertek.com>
2025-05-15 19:01:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-	st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
-	st.write(
+st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
+st.write(
 	)
 import streamlit as st 
 	name_on_order = st.text_input("Name on the Smoothie")
