83072eb07483b17b84c6c8a00b41d88e663f27db
GIT2025-BCT <premakumari.k@bahwancybertek.com>
2025-05-15 18:59:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,5 +34,5 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!', icon="✅")
 
 import requests
-	smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-	sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
