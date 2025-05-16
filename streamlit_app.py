5e244dc69a71060e1a32ff17a2c03651c5e8709f
GIT2025-BCT <premakumari.k@bahwancybertek.com>
2025-05-15 20:12:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,15 +24,14 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_OR
 time_to_insert = st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
-    #st.success('Your Smoothie is ordered!', icon="✅")
-    	
+    #st.success('Your Smoothie is ordered!', icon="✅")  	
 if ingredients_list:  
 	ingredients_string= ' '
 	
 	for fruit_choosen in ingredients_list:
 		ingredients_string += fruit_choosen + ' '
 		st.subheader(fruit_choosen + ' Nutrition Information')
-		smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_choosen)
+		smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/lime")
 		sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_choosen)
