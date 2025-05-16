0ec75513382d27743edce65d9e3cf2e9b4f6133b
GIT2025-BCT <premakumari.k@bahwancybertek.com>
2025-05-16 15:54:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
 	'Choose up to 5 ingredients:'
@@ -34,7 +34,7 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_OR
             values ('""" + ingredients_string + """','""" + name_on_order +"""')"""
 
 
-st.write(my_insert_stmt)
+#st.write(my_insert_stmt)
 #st.stop()
 
 time_to_insert = st.button('Submit Order')
