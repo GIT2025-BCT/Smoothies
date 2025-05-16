b66db0511a58a99e4eb564dc1e143e08d7772922
GIT2025-BCT <premakumari.k@bahwancybertek.com>
2025-05-16 16:22:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write(
 )
 
-
 name_on_order = st.text_input("Name on the Smoothie")
 st.write("The name on the Smoothie is ", name_on_order)
 
@@ -24,23 +23,23 @@ ingredients_list = st.multiselect(
 if ingredients_list:
  ingredients_string= ''
  for fruit_choosen in ingredients_list:
-   ingredients_string += fruit_choosen + ' '
-   smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-   sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-#st.write(ingredients_string) 
+   	ingredients_string += fruit_choosen + ' '
+   	smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+   	sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+  	#st.write(ingredients_string) 
 
- my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
+  	my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
             values ('""" + ingredients_string + """','""" + name_on_order +"""')"""
 
 
-#st.write(my_insert_stmt)
-#st.stop()
+  	#st.write(my_insert_stmt)
+  	#st.stop()
 
- time_to_insert = st.button('Submit Order')
-if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered!', icon="✅")
+ 	time_to_insert = st.button('Submit Order')
+	if time_to_insert:
+    	session.sql(my_insert_stmt).collect()
+    	st.success('Your Smoothie is ordered!', icon="✅")
 	
-# New section to display smoothiefroot nutrition information 
-import requests
-#st.text(smoothiefroot_response.json())
+	# New section to display smoothiefroot nutrition information 
+	import requests
+	#st.text(smoothiefroot_response.json())
